32490130debb42116a1f32182709a8daa3426263
Muhammad-Sami-135 <muhammadsamialam8@gmail.com>
2024-09-11 16:29:16 -0700

    practice questions

---
+++ app.js
@@ -0,0 +1,254 @@
+// Home PDF end
+
+// Chapter 6 (Math Expression II)
+ 
+//  Q1. 
+
+ x= x++;        
+ x= ++x;
+
+//  end
+
+// Q2.
+
+var num= 100;
+var number= num--;
+
+console.log(num);
+
+// end
+
+// Q3.
+
+var x= 50;
+var y= x++;
+
+console.log(x);
+
+// end
+
+// Q4.
+
+var y= 50;
+var z = --y;
+
+console.log(y);
+
+// end
+
+// Q5.
+
+ var num= 10;
+ var newNum= num--;
+
+ console.log(num);
+
+//  end
+
+// Q6.
+
+var num=20;
+var anotherNumber= num++;
+
+console.log(num);
+
+// end
+
+// Q7.
+
+var num = 30;
+number= num++;
+
+alert(num);
+
+// end
+
+// Chapter 7 (Math Expression III)
+
+// Q1
+
+var calculatedNum1 = 2 + (2 * 6);
+
+console.log(calculatedNum1);
+
+// end
+
+// Q2
+
+var calculatedNum2 = (2 + 2) * 6;
+
+console.log(calculatedNum2);
+
+// end
+
+// Q3
+
+var calculatedNum = (2 + 2) * (4 + 2);
+
+console.log(calculatedNum);
+
+// end    
+
+// Q4
+
+var calculatedNum = ((2 + 2) * 4) + 2;
+
+console.log(calculatedNum);
+
+// end
+
+// Q5
+
+var cost = (2 + 2) * (4 + 10);
+
+console.log(cost);
+
+// end
+
+// Q6
+
+var units = 2 + (2 * 4) + 10;
+
+console.log(units);
+
+// end
+
+// Q7
+
+var pressure = 4 / (2 * 4);
+
+console.log(pressure)
+
+// end
+
+// Chapter 8 (Concatenating Text Strings)
+
+// Q1
+
+var num = "2" + "2";
+
+console.log(num);
+
+// end
+
+// Q2
+
+var message = ("Hello," + "Dolly");
+
+alert(message);
+
+// end
+
+// Q3
+
+alert("33" + 3);
+
+// end
+
+// Q4
+
+alert("Pakistan" + " Zindabad");
+
+// end
+
+// Q5
+
+var result = "The answer is " + 42;
+
+// end
+
+// Q6
+
+var firstPart = "Hello, ";
+var secondPart = "world!";
+var message = firstPart + secondPart;
+
+alert(message);
+
+// end
+
+// Chapter 9 (Prompts)
+
+// Q1
+
+var firstName = prompt("Enter first name");
+
+// end
+
+// Q2
+
+var country = prompt("Country?", "China");
+
+// end
+
+// Q3
+
+var yourName = prompt("Enter Your Name");
+
+// end
+
+// Q4
+
+var userInput = prompt("Please enter your favorite color:", "Blue");
+
+// end
+
+// Q5
+
+var promptMessage = "Please enter your age:";
+var defaultResponse = "25";
+var userResponse = prompt(promptMessage, defaultResponse);
+
+alert("You entered: " + userResponse);
+
+// end
+
+// Q6
+
+var userResponse = prompt("What is your favorite fruit?", "Apple");
+
+alert("You entered: " + userResponse);
+
+// end
+
+// Home PDF end
+
+
+// Chhapter no 5
+
+// Q1
+
+var num = 3 + 5;
+document.write("Sum of 3 and 5 is " + num);
+
+// end
+
+// Q2
+
+var sub = 3 - 5;
+console.log("Subcription of 3 and 5 is " + sub);
+
+var mult = 3 * 5;
+console.log("Multiplication of 3 and 5 is " + mult);
+
+var divi = 3 % 5;
+console.log("Division of 3 and 5 is " + divi);
+
+// end
+
+// Q3
+
+// (a)
+var not
+
+// (b)
+var not = 10
+document.write("<br> Value after variable declaration is:." , not)
+
+// (c)
+var number = 3 + 5 + 6
+console.log(number);
+
+// (d)
+var initialValue = 5;
+document.write(initialValue)
